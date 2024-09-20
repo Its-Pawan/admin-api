29b3d79506673e9b8dd65b09c0c1965f92b48eb2
Pawan Joshi <business.pawanjoshi@gmail.com>
2024-09-20 12:38:10 +0530

    still testing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -135,8 +135,8 @@ const loginUser = asyncHandler(async (req, res) => {
     return res
         .status(200)
         .cookie("accessToken", accessToken, options)
-        .cookie("refreshToken", refreshToken, {
-        })
+        .cookie("refreshToken", refreshToken, options
+        )
         .json(
             new ApiResponse(
                 200,
