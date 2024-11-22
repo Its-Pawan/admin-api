880a726b1216a34d9cd4197a8946b7b9b72f4a81
Pawan Joshi <business.pawanjoshi@gmail.com>
2024-11-22 21:54:09 +0530

    _

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import connectDB from "./db/index.js";
 connectDB()
     .then(() => {
         const Port = process.env.PORT || 3000
-        app.on("error", err => console.error("Samething went wrong with app !!!", err))
+        app.on("error", err => console.error("Something went wrong with app !!!", err))
         app.listen(Port, () => console.log("Server is running at post: ", Port))
     })
     .catch(err => console.log("MongoDB connection failed !!!!", err))
